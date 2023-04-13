676ee1f9ee136f245980197c3332b75cf97ea517
CrlTomato <ivancabrerarj@gmail.com>
2023-04-12 22:37:51 -0600

    Añadido nuevo escenario (Secta), patrones de tiles, cambios de colores y algunas modificaciones a los sprites

---
+++ Valiositos/Patterns/naturaleza.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="naturaleza" tilewidth="16" tileheight="16" tilecount="20" columns="4">
+ <image source="../Seleccion/Cositillas.png" width="64" height="80"/>
+</tileset>
