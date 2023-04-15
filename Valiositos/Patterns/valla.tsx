b1bdd66e529db4d5e5b688e8c6d1a83522378375
CrlTomato <ivancabrerarj@gmail.com>
2023-04-14 19:16:23 -0600

    Alch no m acuerdo q hice

---
+++ Valiositos/Patterns/valla.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="valla" tilewidth="16" tileheight="16" tilecount="12" columns="4">
+ <image source="../Seleccion/generic-rpg-fence-complete.png" width="67" height="62"/>
+</tileset>
